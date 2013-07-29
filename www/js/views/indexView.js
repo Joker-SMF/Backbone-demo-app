2e41d3444d5ca978ed831d6b543ce4a8a89cf64a
Joker-SMF <makerzzz.zzzz@gmail.com>
2013-07-29 20:52:03 +0530

    lets add some css and fluidity

--- www/js/views/indexView.js
+++ www/js/views/indexView.js
@@ -3,9 +3,8 @@
 
 	App.views.indexItemView = Backbone.Marionette.ItemView.extend({
 		template: "#individual_book-template",
-		tagName: 'tr',
 		events: {
-			'click .book_name' : 'bookName'
+			'click .book_info' : 'bookName'
 		},
 
 		initialize: function() {
@@ -22,11 +21,9 @@
 	});
 
 	App.views.indexView = Backbone.Marionette.CompositeView.extend({
-		tagName: "table",
-		id: "books_listing",
 		template: "#books_listing-template",
 		itemView: App.views.indexItemView,
-		customEl: 'tbody.individual_book',
+		customEl: '.individual_book',
 
 		initialize: function() {
 		},
